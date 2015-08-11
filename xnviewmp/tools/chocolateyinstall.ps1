eeafb5ff1ebd380c3bebf6526191faaf90305812
nwgat <anyone@nwgat.net>
2015-08-11 21:09:47 +0200

    XnViewMP
    Newer version of XnView

---
+++ xnviewmp/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'xnviewmp'
+$installerType = 'exe'
+$url = 'http://download.xnview.com/XnViewMP-win.exe'
+$url64 = 'http://download.xnview.com/XnViewMP-win-x64.exe'
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
