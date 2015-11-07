f5fdb7bfd71db24aa3f2576e89f6d27b6d2f19ed
nwgat <anyone@nwgat.net>
2015-11-07 16:05:30 +0100

    .
    .

---
+++ hdtunepro/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'Package Name'
+$installerType = 'exe'
+$url = 'http://www.hdtune.com/files/hdtunepro_560_trial.exe'
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0,3010)
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
