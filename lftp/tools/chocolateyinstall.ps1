0837e8d4967b993f3adbd0411a6199b140052510
nwgat <github@nwgat.net>
2015-11-10 21:00:58 +0100

    updated to lftp 4.6.4

--- lftp/tools/chocolateyinstall.ps1
+++ lftp/tools/chocolateyinstall.ps1
@@ -1,10 +1,7 @@
-﻿$packageName = 'lftp' # arbitrary name for the package, used in messages
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url  = 'http://lftp.nwgat.ninja/lftp-4.6.3a/lftp-4.6.3a.win32-openssl.7z'
-$url64 ='http://lftp.nwgat.ninja/lftp-4.6.3a/lftp-4.6.3a.win32-openssl.7z'
-
-
-Install-ChocolateyZipPackage @packageArgs
-
-## Download and unpack a zip file
-##Install-ChocolateyZipPackage $packageName $url $toolsDir [$url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
+$packageName = 'lftp'
+$url = 'https://lftp.nwgat.ninja/lftp-4.6.4/lftp-4.6.4.win32-openssl.zip'
+$url64 = 'https://lftp.nwgat.ninja/lftp-4.6.4/lftp-4.6.4.win64-openssl.zip'
+ 
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+ 
+Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
