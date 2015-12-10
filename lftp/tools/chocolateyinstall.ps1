15ada2433d74b30cc23aaf352a192352b3861cca
nwgat <anyone@nwgat.net>
2015-12-10 16:19:57 +0100

    .
    .

--- lftp/tools/chocolateyinstall.ps1
+++ lftp/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'lftp'
-$url = 'https://lftp.nwgat.ninja/lftp-4.6.4/lftp-4.6.5.win32-openssl.zip'
-$url64 = 'https://lftp.nwgat.ninja/lftp-4.6.4/lftp-4.6.5.win64-openssl.zip'
+$url = 'https://lftp.nwgat.ninja/lftp-4.6.5/lftp-4.6.5.win32-openssl.zip'
+$url64 = 'https://lftp.nwgat.ninja/lftp-4.6.5/lftp-4.6.5.win64-openssl.zip'
  
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
  
