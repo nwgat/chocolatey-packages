76e041eee0b4b0636468e91063a810c114287306
nwgat <anyone@nwgat.net>
2016-02-15 00:17:58 +0100

    switched to cdn (lftp), added openshot windows release

--- lftp/tools/chocolateyinstall.ps1
+++ lftp/tools/chocolateyinstall.ps1
@@ -1,6 +1,9 @@
 $packageName = 'lftp'
 $url = 'https://lftp.nwgat.ninja/lftp-4.6.5/lftp-4.6.5.win32-openssl.zip'
+$url = 'http://cdn.nwgat.ninja/lftp/win32/lftp-4.6.5.win32-openssl.zip'
 $url64 = 'https://lftp.nwgat.ninja/lftp-4.6.5/lftp-4.6.5.win64-openssl.zip'
+$url64 = 'http://cdn.nwgat.ninja/lftp/win64/lftp-4.6.5.win64-openssl.zip'
+
  
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
  
---
+++ openshot/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'Package Name'
+$installerType = 'msi'
+$url = 'http://releases.openshot.org/windows/OpenShot+Video+Editor-2.0.6-win32.msi'
+$url64 = 'http://releases.openshot.org/windows/OpenShot+Video+Editor-2.0.6-win32.msi'
+$silentArgs = '/quiet /norestart'
+$validExitCodes = @(0,3010)
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64  -validExitCodes $validExitCodes
