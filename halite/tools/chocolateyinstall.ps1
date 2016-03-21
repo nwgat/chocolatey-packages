f3a07e4e6d3986825139e1141a64dc2248d3dcc2
nwgat <anyone@nwgat.net>
2016-03-21 22:32:57 +0100

    halite 4.0.4

---
+++ halite/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'halite'
+$installerType = 'msi'
+$url = 'https://github.com/Eoinocal/Halite/releases/download/Release-0.4.0.4/Halite-0.4.0.4-x86.msi'
+$url64 = 'https://github.com/Eoinocal/Halite/releases/download/Release-0.4.0.4/Halite-0.4.0.4-x64.msi'
+$silentArgs = '/quiet /norestart'
+$validExitCodes = @(0,3010)
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64  -validExitCodes $validExitCodes
