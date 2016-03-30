5f0fd568164527b9a7bea09d862bd4a8351acc02
nwgat <anyone@nwgat.net>
2016-03-30 08:11:15 +0200

    update

--- openshot/tools/chocolateyinstall.ps1
+++ openshot/tools/chocolateyinstall.ps1
@@ -1,7 +1,6 @@
 ﻿$packageName = 'openshot'
 $installerType = 'msi'
 $url = 'http://releases.openshot.org/windows/OpenShot-2.0.7.msi'
-$url64 = 'http://releases.openshot.org/windows/OpenShot-2.0.7.msi'
 $silentArgs = '/quiet /norestart'
 $validExitCodes = @(0,3010)
 
