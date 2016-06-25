91f74843e874682ef2ca14c6fa3e1a1828727193
nwgat <github@nwgat.net>
2016-06-25 16:16:05 +0200

    update to 4.6.6

--- lftp/tools/chocolateyinstall.ps1
+++ lftp/tools/chocolateyinstall.ps1
@@ -1,8 +1,5 @@
 $packageName = 'lftp'
 $url = 'https://cdn.nwgat.ninja/lftp/win32/lftp-4.6.6.win32-openssl.zip'
 $url64 = 'http://cdn.nwgat.ninja/lftp/win64/lftp-4.6.6.win64-openssl.zip'
-
- 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
- 
 Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
