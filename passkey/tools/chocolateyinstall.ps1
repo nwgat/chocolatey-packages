dd98983b4d0e114e3cd99b352686593e40c3c58e
nwgat <hello@nwgat.net>
2016-07-14 21:42:19 +0200

    passkey

---
+++ passkey/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'passkey'
+$installerType = 'exe'
+$url = 'http://www.dvdfab.cn/mlink/download.php?g=PASSKEY'
+$url64 = 'http://www.dvdfab.cn/mlink/download.php?g=PASSKEY'
+$silentArgs = '/VERYSILENT /NORESTART'
+$validExitCodes = @(0,3010)
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64  -validExitCodes $validExitCodes
