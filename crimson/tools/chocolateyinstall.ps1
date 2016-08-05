d972add8562b804ef2d88e0234ab3984252664c8
nwgat <hello@nwgat.net>
2016-08-05 19:54:59 +0200

    new stuff

---
+++ crimson/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'crimson'
+$installerType = 'exe'
+$url = 'E:\Downloads\non-whql-64bit-radeon-software-crimson-16.3.1-win10-win8.1-win7-march16.exe'
+$url64 = 'E:\Downloads\non-whql-64bit-radeon-software-crimson-16.3.1-win10-win8.1-win7-march16.exe'
+$silentArgs = '/S /D=C:\AMD\non-whql-64bit-radeon-software-crimson-16.3.1-win10-win8.1-win7-march16'
+$silentArgs = '-INSTALL'
+$validExitCodes = @(0,3010)
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64  -validExitCodes $validExitCodes
--- lftp/tools/chocolateyinstall.ps1
+++ lftp/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'lftp'
-$url = 'https://cdn.nwgat.ninja/lftp/win32/lftp-4.6.6.win32-openssl.zip'
-$url64 = 'http://cdn.nwgat.ninja/lftp/win64/lftp-4.6.6.win64-openssl.zip'
+$url = 'http://d2tavyk6mzr5pc.cloudfront.net/lftp/win32/lftp-4.6.6.win32-openssl.zip'
+$url64 = 'http://d2tavyk6mzr5pc.cloudfront.net/lftp/win64/lftp-4.6.6.win64-openssl.zip'
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
