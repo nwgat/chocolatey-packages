e0b0fa3f36f8d7b2c93fabbd1e3ca4079fc443d1
nwgat <hello@nwgat.net>
2017-01-06 18:24:28 +0100

    ...

--- tinc-1.0/tools/chocolateyinstall.ps1
+++ tinc-1.0/tools/chocolateyinstall.ps1
@@ -1,5 +1,8 @@
 ﻿$packageName = 'tinc'
 $installerType = 'exe'
+$url = 'https://www.tinc-vpn.org/packages/windows/tinc-1.0.30-install.exe'
+$url64 = 'https://www.tinc-vpn.org/packages/windows/tinc-1.0.30-install.exe'
+$checksum = '90A68BDD0B617159BC6E70FDE5B54B4D'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
