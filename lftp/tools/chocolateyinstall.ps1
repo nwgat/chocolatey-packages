b43606724c0e0f40ef33d3ce91bec9ce470bd34f
nwgat <github@nwgat.net>
2020-06-18 06:41:59 +0200

    Update chocolateyinstall.ps1

--- lftp/tools/chocolateyinstall.ps1
+++ lftp/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'lftp'
-$url = 'https://f001.backblazeb2.com/file/nwgat-cdn/lftp/win32/lftp-4.8.0.win32-openssl.zip'
-$url64 = 'https://f001.backblazeb2.com/file/nwgat-cdn/lftp/win64/lftp-4.8.0.win64-openssl.zip'
+$url = 'https://f001.backblazeb2.com/file/nwgat-cdn/lftp/win32/lftp-4.9.1.win32-openssl.zip'
+$url64 = 'https://f001.backblazeb2.com/file/nwgat-cdn/lftp/win64/lftp-4.9.1.win64-openssl.zip'
 $checksum = 'A88A1A694A907CE7226836F8CB12A919'
 $checksum64 = '0C19C8DD3D296C8253116D4AD4877F79'
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
