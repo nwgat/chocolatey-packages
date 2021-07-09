c0a16833c1e143076d1434ccec91e0e53205b085
attero <service-github@attero.me>
2021-07-09 14:57:24 +0000

    don't create shims for unwanted helper programs

--- lftp/tools/chocolateyinstall.ps1
+++ lftp/tools/chocolateyinstall.ps1
@@ -4,4 +4,9 @@ $url64 = 'https://f001.backblazeb2.com/file/nwgat-cdn/lftp/win64/lftp-4.9.1.win6
 $checksum = 'A88A1A694A907CE7226836F8CB12A919'
 $checksum64 = '0C19C8DD3D296C8253116D4AD4877F79'
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+out-file -filepath sh.exe.ignore  -InputObject ""
+out-file -filepath bash.exe.ignore  -InputObject ""
+out-file -filepath ssh.exe.ignore  -InputObject ""
+
 Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64" $checksum
