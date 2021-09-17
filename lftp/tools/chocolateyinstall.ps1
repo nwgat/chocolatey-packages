8c926821b9048bb312096c5de2e0f65f8a4e4690
attero <service-github@attero.me>
2021-09-17 09:07:03 +0000

    Update chocolateyinstall.ps1
    sorry that didn't work as intended should be fixed now
    
    see:
    https://github.com/digitalcoyote/chocolatey-packages/blob/master/drawio/tools/chocolateyinstall.ps1#L25

--- lftp/tools/chocolateyinstall.ps1
+++ lftp/tools/chocolateyinstall.ps1
@@ -3,10 +3,13 @@ $url = 'https://f001.backblazeb2.com/file/nwgat-cdn/lftp/win32/lftp-4.9.2.win32-
 $url64 = 'https://f001.backblazeb2.com/file/nwgat-cdn/lftp/win64/lftp-4.9.2.win64-openssl.zip'
 $checksum = 'A88A1A694A907CE7226836F8CB12A919'
 $checksum64 = '0C19C8DD3D296C8253116D4AD4877F79'
-$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-out-file -filepath sh.exe.ignore  -InputObject ""
-out-file -filepath bash.exe.ignore  -InputObject ""
-out-file -filepath ssh.exe.ignore  -InputObject ""
+Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir" "$url64" $checksum
 
-Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64" $checksum
+#Create .ignore files so chocolatey does not shim the support programs
+$filestoignore = Get-ChildItem $toolsDir -include *.exe -exclude lftp.exe
+
+foreach ($file in $filestoignore) {
+    New-Item "$file.ignore" -type file -force | Out-Null
+}
